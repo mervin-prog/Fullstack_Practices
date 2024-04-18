893b934308e4b36203752914fcc5b7890bf32b3d
mervin <mervinrenie@gmail.com>
2024-04-18 11:17:01 +0530

    changes in  Task 20

--- Task-20_mar18/index.js
+++ Task-20_mar18/index.js
@@ -2,20 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port=3000;
+const port=process.env.PORT;
 const saltRounds=10;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "user",
-    password: "mervin",
-    port: 5432,
-  });
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
 db.connect();
 
   app.get("/", (req, res) => {
