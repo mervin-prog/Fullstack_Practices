0077183a4304bc54b7d292602c27db597d491a63
Mervin <91684170+mervin-prog@users.noreply.github.com>
2024-04-27 17:06:34 +0530

    Delete Task-37&38_April23&24 directory

--- Task-37&38_April23&24/queries.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE details (
-    user_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    role_name VARCHAR(50),
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(100)
-);
